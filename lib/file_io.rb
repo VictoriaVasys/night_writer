a29d8d8d586abb637e9a7bd06ef1b622ad875d78
VictoriaVasys <vvasys@gmail.com>
2017-02-09 08:55:26 -0700

    translations operable & cleaned-up

---
+++ lib/file_io.rb
@@ -0,0 +1,16 @@
+class FileIO
+  def read
+    filename = ARGV[0]
+    File.read(filename)
+  end
+  
+  def write(translation)
+    filename = ARGV[1]
+    new_braille = File.open(filename, "w")
+    new_braille.write(translation)
+    new_braille.close
+    read_new = File.open(filename, "r")
+    number_of_characters = read_new.read.length
+    puts "Created '#{ARGV[1]}' containing #{number_of_characters} characters"
+  end
+end
--- lib/night_read.rb
+++ lib/night_read.rb
@@ -1,14 +1,14 @@
-require './lib/file_reader.rb'
+require './lib/file_io.rb'
 require './lib/braille_alphaneumeric'
 
 
-class BrailleToEnglish
+class NightRead
   include BrailleAlphaneumeric
-  attr_reader :file_reader, :braille_alphabet, :english_letters, :braille_letters, :reader, :lines_parsed, :english_letters_final, :english
+  attr_reader :file_io, :braille_alphabet, :english_letters, :braille_letters, :lines_parsed, :english_letters_final, :english
   attr_accessor :line_1, :line_2, :line_3, :line_1_braille, :line_2_braille, :line_3_braille
   
   def initialize
-    @reader = FileReader.new
+    @file_io = FileIO.new
     @braille_alphabet = BrailleAlphaneumeric.braille_alphabet
     @lines_parsed = []
     @line_1 = []
@@ -23,9 +23,9 @@ class BrailleToEnglish
   end
   
   def encode_file_to_english
-    braille = reader.read
+    braille = file_io.read
     @english = encode_to_english(braille)
-    encode_english_translation_to_file
+    file_io.write(english)
   end
   
   def encode_to_english(input)
@@ -59,26 +59,36 @@ class BrailleToEnglish
   end
   
   def capitalize_letters
-    @english_letters.each_with_index do |letter, i|
-      if letter == "caps"
+    @english_letters.each_with_index do |character, i|
+      if character == "caps"
         english_letters_final << english_letters[i + 1].upcase
         i += 2
       elsif @english_letters[i - 1] == "caps"
+      elsif character == "#"
+        i += 1
+        translate_numbers(english_letters[i], i)
+        i = new_i
       else
-        english_letters_final << letter
-        
+        english_letters_final << character
       end
     end
     make_single_string
+  end
+  
+  def translate_numbers(english_letter, new_i)
+    until english_letters[i] == " "
+      english_number = braille_letters_to_numbers[english_letters[i]]
+      english_letters_final << english_number
+      @new_i += 1 ## need to work on this
+      translate_numbers(english_letters[i], i)
+    end
   end
   
   def make_single_string
     english_letters_final.join
   end
   
-  def encode_english_translation_to_file
-    new_english = File.open(ARGV[1], "w")
-    new_english.write(english)
-  end
-  
 end
+
+nr = NightRead.new 
+nr.encode_file_to_english
